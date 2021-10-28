97063a74ba7013ae9299a313b6fb6e8063138a98
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2021-10-28 16:49:29 +1000

    added Scratch and Win inline JS

--- js/git-update.js
+++ js/git-update.js
@@ -1,3 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Github stuff');
 });
+
+window.addEventListener('scroll', () => {
+    new ParallaxEffect('.scratch-and-win .horizontal-line');
+});

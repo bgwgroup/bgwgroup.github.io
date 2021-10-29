7e550f2666f84575f435ab9d4b2c35fd7d699241
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2021-10-29 15:06:17 +1000

    added dynamic year object to color comp pages

--- js/git-update.js
+++ js/git-update.js
@@ -1,7 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
+    try{
+        colouringCompetitionYear();
+    }catch(error){}
 });
 
 window.addEventListener('scroll', () => {
     new ParallaxEffect('.scratch-and-win .horizontal-line');
 });
+
+
+function colouringCompetitionYear(){
+    let coloringCompTableSpan = document.querySelector('.coloring-competition .coloring-table-row:nth-child(1) > span:nth-child(2)');
+    coloringCompTableSpan.innerHTML = coloringCompTableSpan.innerHTML + " " + new Date().getFullYear();
+}

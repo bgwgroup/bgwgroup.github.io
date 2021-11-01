f78164a73f9c1d9cb13d0db85f30b02b2297bd63
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2021-11-02 07:07:05 +1000

    added search filter for favourite wishlist

--- js/git-update.js
+++ js/git-update.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     try{
         colouringCompetitionYear();
+        favouriteSearchFilter();
     }catch(error){}
 });
 
@@ -12,4 +13,20 @@ window.addEventListener('scroll', () => {
 function colouringCompetitionYear(){
     let coloringCompTableSpan = document.querySelector('.coloring-competition .coloring-table-row:nth-child(1) > span:nth-child(2)');
     coloringCompTableSpan.innerHTML = coloringCompTableSpan.innerHTML + " " + new Date().getFullYear();
+}
+function favouriteSearchFilter(){
+    let favSeachForm = document.querySelector('.fav-list-search-filter #favListSearchFilter');
+    let favGridItem = document.querySelectorAll('#favourite_list .fav_grid');
+
+    favSeachForm.addEventListener('keyup', () => {
+        let favSearchFormValue = favSeachForm.value.toUpperCase();
+        for(let i = 0; i < favGridItem.length; i++){
+            let textValue = favGridItem[i].textContent || favGridItem[i].innerText[i];
+            if(textValue.toUpperCase().indexOf(favSearchFormValue) > -1){
+                favGridItem[i].style.display = "";
+            }else{
+                favGridItem[i].style.display = "none";
+            }
+        } 
+    });
 }

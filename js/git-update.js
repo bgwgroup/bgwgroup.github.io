9fc7de25de5c47dfe846517d74fc166df1eddd90
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2022-07-29 15:39:10 +1000

    added Parallax Effect for Samios 75th anniversary

--- js/git-update.js
+++ js/git-update.js
@@ -996,6 +996,9 @@ CountDownTimer.prototype.HTMLElement = (element, string) => {
  */
 window.addEventListener('load', () => {
     new SamAnnivesary();
+    if (location.origin.match(/shop.samios.net.au/gi)) {
+        new ParallaxEffect('.sfy-section-content .sfy-content .sfy-brand');
+    }
 });
 
 function SamAnnivesary() {

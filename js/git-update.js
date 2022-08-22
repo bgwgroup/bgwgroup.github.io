7ad0401b54e329be0f630ec0cd224b6d38dfb827
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2022-08-22 10:20:44 +1000

    fixed tenary operator syntax issue

--- js/git-update.js
+++ js/git-update.js
@@ -1147,10 +1147,10 @@ FlutterJS.prototype.getCartCount = () => {
     }
 };
 FlutterJS.prototype.checkErrorLogin = () => {
-    if(window.checkErrorLogin){
+    if (window.checkErrorLogin) {
         let errorLoginElement = document.querySelector(".alert.error.getAccAlert.danger-alert > div");
-        if(errorLoginElement != undefined){
-            return (errorLoginElement.innerHTML.length == 0) window.checkErrorLogin.postMessage("true") : window.checkErrorLogin.postMessage("false");
+        if (errorLoginElement != undefined) {
+            return (errorLoginElement.innerHTML.length == 0) ? window.checkErrorLogin.postMessage("true") : window.checkErrorLogin.postMessage("false");
         }
     }
 };

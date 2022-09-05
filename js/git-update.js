945e3944355318dc5265669ccda06ffcafa09847
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2022-09-05 11:49:42 +1000

    added window open for href link for Flutter

--- js/git-update.js
+++ js/git-update.js
@@ -1183,6 +1183,7 @@ FlutterJS.prototype.getHrefLink = () => {
                     let regex = new RegExp(mediaType);
                     if (anchor.match(regex)) {
                         hrefLinks[i].addEventListener('click', () => {
+                            window.open(anchor);
                             return window.hrefLink.postMessage(anchor);
                         });
                     }

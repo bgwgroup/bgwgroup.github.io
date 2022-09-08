8bbd12aa36f5b9d0910a79d0e09b7fd12c5a690a
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2022-09-08 16:02:12 +1000

    added dynamic position absolute top calculation for CIO Sherriff and CNW

--- js/git-update.js
+++ js/git-update.js
@@ -1191,3 +1191,20 @@ FlutterJS.prototype.getHrefLink = () => {
         }
     }
 };
+/**
+ * Clipsal Iconic Outdoor Floating Text
+ */
+['change', 'load'].forEach((event) => {
+    window.addEventListener(event, () => {
+        if (location.origin.match(/shop.sherriff.com.au/gi) || location.origin.match(/shop.cnw.com.au/gi)) {
+            let nav = document.querySelector('#sheBody .headerContent nav') || document.querySelector('.headerContent .cnw-header-navigation');
+            if (nav != undefined) {
+                let bounds = nav.getBoundingClientRect();
+                let cioStickyInfo = document.querySelector('.sticky-info');
+                if (cioStickyInfo != undefined) {
+                    cioStickyInfo.style.top = `${bounds.bottom}px`;
+                }
+            }
+        }
+    });
+});

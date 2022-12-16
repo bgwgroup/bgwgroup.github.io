c95a28737fa8056d2ef0248949879b29e177a7c9
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2022-12-16 10:28:13 +1000

    removed maintenance banners from website

--- js/git-update.js
+++ js/git-update.js
@@ -1324,7 +1324,7 @@ function clearAcStoreinformationSelector(nodeList, selector) {
 window.addEventListener('DOMContentLoaded', () => {
     if (location.href.match(/cnw.com.au/) || location.href.match(/sherriff.com.au/) || location.href.match(/samios.net.au/)) {
         try {
-            new MaintenanceBanner();
+            //new MaintenanceBanner();
         } catch (e) {}
     }
 });

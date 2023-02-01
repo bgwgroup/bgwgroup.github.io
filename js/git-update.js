bfc98ccb68d49e94adcf975be0b79136707d4a43
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2023-02-01 11:13:40 +1000

    added banner for Flow Pricing change to be happening later at night on Feb 1

--- js/git-update.js
+++ js/git-update.js
@@ -1324,7 +1324,7 @@ function clearAcStoreinformationSelector(nodeList, selector) {
 window.addEventListener('DOMContentLoaded', () => {
     if (location.href.match(/cnw.com.au/) || location.href.match(/sherriff.com.au/) || location.href.match(/samios.net.au/)) {
         try {
-            //new MaintenanceBanner();
+            new MaintenanceBanner();
         } catch (e) {}
     }
 });
@@ -1344,7 +1344,7 @@ MaintenanceBanner.prototype.renderBanner = () => {
     let sapMaintenanceBanner = document.createElement('div');
     sapMaintenanceBanner.className = 'sap-maintenance-banner';
     sapMaintenanceBanner.innerHTML = `
-        <span>The website will be undergoing maintenance on the <strong>15<sup>th</sup> of December 2022 between 8:00pm - 10:00pm AEST</strong>. Pricing and other functionality may be affected during this period. We apologise for any inconveniences caused. For any urgent assistance, please contact the support team on <a href="mailto:online.support@bgwgroup.com.au">online.support@bgwgroup.com.au</a>.</span>
+        <span>The website will be undergoing maintenance on the <strong>1<sup>st</sup> of February between 7:00pm - 12:00pm AEST</strong>. Pricing and other functionality may be affected during this period. We apologise for any inconveniences caused. For any urgent assistance, please contact the support team on <a href="mailto:online.support@bgwgroup.com.au">online.support@bgwgroup.com.au</a>.</span>
     `;
     document.body.appendChild(sapMaintenanceBanner);
 };

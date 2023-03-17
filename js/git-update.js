60b809829d165e504e03f4f2951e9c9383f1c2e8
Bongani Mkonto <bmkonto@bgwgroup.com.au>
2023-03-18 01:10:13 +1000

    added full screen banner with styles

--- js/git-update.js
+++ js/git-update.js
@@ -1362,8 +1362,9 @@ MaintenanceBanner.prototype.fullScreenBanner = () => {
     let fullScreenBanner = document.createElement('div');
     fullScreenBanner.className = 'full-screen-banner';
     fullScreenBanner.innerHTML = `
-    <span>The website will be undergoing maintenance between 7:00pm AEST the 17th of March and 7:00am AEST the 19th of March. Pricing and account access will be unavailable during this period, we apologise for any inconveniences caused. For any urgent assistance, please contact the support team via <a href="mailto:online.support@bgwgroup.com.au">online.support@bgwgroup.com.au</a></span>
+    <span style="position: absolute;top: 50%;left: 0;right: 0;width: 100%;padding: 2rem;transform: translateY(-50%);line-height: 1.5rem;">The website will be undergoing maintenance between 7:00pm AEST the 17th of March and 7:00am AEST the 19th of March. Pricing and account access will be unavailable during this period, we apologise for any inconveniences caused. For any urgent assistance, please contact the support team via <a href="mailto:online.support@bgwgroup.com.au" style="color: #C00;">online.support@bgwgroup.com.au</a></span>
     `;
+    fullScreenBanner.setAttribute('style', `position: fixed;top: 0;left: 0;width: 100dvw;height: 100dvh;background: #FFF;z-index: 9999999;`);
     document.body.appendChild(fullScreenBanner);
 }
 

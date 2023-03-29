c39323e13db7a329abd7258002f18bf822cf4d01
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-03-29 13:33:14 +1000

    send redeemable points to backend

--- js/git-update.js
+++ js/git-update.js
@@ -1706,11 +1706,11 @@ function getCustomerPointsOnSearch() {
 
 let redeemablePoints = undefined;
 
-function getAccountDetails() {
-
+function getAccountDetails(){
+    
     let URL = "https://bgwgroup.com.au/sammymas2023/get-total-customer-points.php";
     let eligibleURL = 'https://bgwgroup.com.au/sammymas2023/get-prize-list-json.php';
-
+    
     let redeemFormHidden = document.querySelector('#redeemForm .form-hidden');
     let redeemAccount = document.querySelector('#redeemForm input#redeemAccount');
     let redeemPoints = document.querySelector('#redeemForm #redeemPoints');
@@ -1718,94 +1718,91 @@ function getAccountDetails() {
     let redeemAccountName = document.querySelector('#redeemForm #redeemAccountName');
     let redeemList = document.querySelector('#redeemForm select#redeemList');
     let redeemError = document.querySelector('#redeemForm #redeemError span');
-
+    
     let accountPoints = undefined;
+    
 
-    if (redeemAccount != undefined) {
-        redeemAccount.addEventListener('keyup', () => {
-            if (redeemAccount.value.length >= 2) {
-                fetch(URL + `?accountNumber=${redeemAccount.value}`)
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((data) => {
-                        for (let i = 0; i < data.length; i++) {
-                            accountPoints = data[i]['total_points'];
+    redeemAccount.addEventListener('keyup', () => {
+        if(redeemAccount.value.length >= 2){
+            fetch(URL + `?accountNumber=${redeemAccount.value}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                for(let i = 0; i < data.length; i++){
+                    accountPoints = data[i]['total_points'];
 
-                            // clear hidden elements
-                            redeemFormHidden.innerHTML = '';
+                    // clear hidden elements
+                    redeemFormHidden.innerHTML = '';
 
-                            let accountID = document.createElement('input');
-                            accountID.type = 'hidden';
-                            accountID.id = 'prize_id';
-                            accountID.setAttribute('name','prize_id');
-                            accountID.value = data[i]['id'];
-        
-                            let accountNumber = document.createElement('input');
-                            accountNumber.type = 'hidden';
-                            accountNumber.id = 'account_number';
-                            accountNumber.setAttribute('name','account_number');
-                            accountNumber.value = data[i]['account_number'];
-        
-                            let accountHomeBranch = document.createElement('input');
-                            accountHomeBranch.type = 'hidden';
-                            accountHomeBranch.id = 'home_branch';
-                            accountHomeBranch.setAttribute('name','home_branch');
-                            accountHomeBranch.value = data[i]['branch'];
-        
-                            redeemFormHidden.appendChild(accountID);
-                            redeemFormHidden.appendChild(accountNumber);
-                            redeemFormHidden.appendChild(accountHomeBranch);
-        
-                            // add account name to field
-                            redeemAccountName.value = data[i]['account_name'];
-        
-                            // add customer points
-                            let redeemedPoints = data[i]['redeemed_points'] || 0;
-                            redeemPoints.value = accountPoints - redeemedPoints;
-                            redeemablePoints = accountPoints - redeemedPoints;
-                            if ( redeemPoints.value < 10) {
-                                redeemError.innerHTML = '';
-                                redeemError.innerHTML = 'Sorry but you do not have enough points';
-                            }  
-        
+                    let accountID = document.createElement('input');
+                    accountID.type = 'hidden';
+                    accountID.id = 'prize_id';
+                    accountID.setAttribute('name','prize_id');
+                    accountID.value = data[i]['id'];
 
+                    let accountNumber = document.createElement('input');
+                    accountNumber.type = 'hidden';
+                    accountNumber.id = 'account_number';
+                    accountNumber.setAttribute('name','account_number');
+                    accountNumber.value = data[i]['account_number'];
+
+                    let accountHomeBranch = document.createElement('input');
+                    accountHomeBranch.type = 'hidden';
+                    accountHomeBranch.id = 'home_branch';
+                    accountHomeBranch.setAttribute('name','home_branch');
+                    accountHomeBranch.value = data[i]['branch'];
+
+                    redeemFormHidden.appendChild(accountID);
+                    redeemFormHidden.appendChild(accountNumber);
+                    redeemFormHidden.appendChild(accountHomeBranch);
+
+                    // add account name to field
+                    redeemAccountName.value = data[i]['account_name'];
+
+                    // add customer points
+                    let redeemedPoints = data[i]['redeemed_points'] || 0;
+                    redeemPoints.value = accountPoints - redeemedPoints;
+                    redeemablePoints = accountPoints - redeemedPoints;
+                    if ( redeemPoints.value < 10) {
+                        redeemError.innerHTML = '';
+                        redeemError.innerHTML = 'Sorry but you do not have enough points';
+                    }  
+
+                }
+                // search for list of eligible products
+                fetch(eligibleURL)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((products) => {
+                    // clear select element 
+                    redeemList.innerHTML = '';
+                    for(let p = 0; p < products.length; p++){
+                        // get eligible products based on points less than account points
+                        if( parseInt(products[p]['points']) <= Math.ceil(parseInt(redeemPoints.value)) ){
+                            if (p === 0){
+                                redeemList.options[redeemList.options.length] = new Option('Select Prize...');
+                            }
+                            redeemAccountDetails.classList.remove('form-list-hidden');
+                            redeemList.options[redeemList.options.length] = new Option(products[p]['description'] + ' - ' + products[p]['points'] + ' points',products[p]['id']);
                         }
-                        // search for list of eligible products
-                        fetch(eligibleURL)
-                            .then((response) => {
-                                return response.json();
-                            })
-                            .then((products) => {
-                                // clear select element 
-                                redeemList.innerHTML = '';
+                    }
 
-                                for (let p = 0; p < products.length; p++) {
-                                    // get eligible products based on points less than account points
-                                    if( parseInt(products[p]['points']) <= Math.ceil(parseInt(redeemPoints.value)) ){
-                                        if (p === 0){
-                                            redeemList.options[redeemList.options.length] = new Option('Select Prize...');
-                                        }
-                                        redeemAccountDetails.classList.remove('form-list-hidden');
-                                        redeemList.options[redeemList.options.length] = new Option(products[p]['description'] + ' - ' + products[p]['points'] + ' points',products[p]['id']);
-                                    }
-                                }
+                })
+                .catch((error) => {});
 
-                            })
-                            .catch((error) => {});
+            })
+            .catch((error) => {});
 
-                    })
-                    .catch((error) => {});
-
-            } else {
-                // if account number input field if empty, clear form values
-                redeemAccountDetails.classList.add('form-list-hidden');
-                redeemList.innerHTML = '';
-                redeemAccountName.value = '';
-                redeemError.innerHTML = '';
-            }
-        });
-    }
+        }else{
+            // if account number input field if empty, clear form values
+            redeemAccountDetails.classList.add('form-list-hidden');
+            redeemList.innerHTML = '';
+            redeemAccountName.value = '';
+            redeemError.innerHTML = '';
+        }
+    });
 }
 
 function submitRedemptionForm() {
@@ -1826,7 +1823,7 @@ function submitRedemptionForm() {
         redeemSubmit.addEventListener('click', (event) => {
             event.preventDefault();
             redeemSubmit.setAttribute('disabled', 'disabled');
-
+            redeemSubmit.classList.add('spinnerLoading');
             let formData = new FormData();
             formData.append('prize_id', prizeID);
             formData.append('account_number', document.querySelector('#account_number').value);
@@ -1841,6 +1838,7 @@ function submitRedemptionForm() {
                     return response.json();
                 })
                 .then((formResponse) => {
+                    // redeemSubmit.removeAttribute('disabled', 'disabled');
                     redeemSubmit.classList.remove('spinnerLoading');
                     if (formResponse.email_sent === true ) {
                         document.querySelector('#redeemForm').reset();

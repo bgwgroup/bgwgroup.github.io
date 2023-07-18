42f6522b3cf35b201fd238787eff88a41078e3f9
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-07-18 16:25:47 +1000

    add animation for sammy showcase 2023

--- js/git-update.js
+++ js/git-update.js
@@ -2070,6 +2070,27 @@ const mapLinkToCategory = (name, link) => {
     }
 };
 
+
+// SammyShowcase 2023
+
+let leftBlind = document.getElementById('blind-left')
+let rightBlind = document.getElementById('blind-right')
+let sammyTextCtn = document.querySelector('.sammy-shout-wordings-ctn')
+let bubbles = document.querySelectorAll('.bubbles span')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    if(!!leftBlind) {
+
+        leftBlind.style.left = value * -1.5 +'px';
+        rightBlind.style.right = value * -1.5 +'px';
+        sammyTextCtn.style.top = value * -0.8 + 500+ 'px';
+        Array.from(bubbles).map((bubble, index) => {
+            bubble.style.top = value * -0.8 + 'px';
+        })
+    }
+})
+
 // temp fix samios appliances link
 if (window.location.href.indexOf("samios.net.au") != -1){
     const getApplianceLink = document.querySelector('[title="Applicances"]');

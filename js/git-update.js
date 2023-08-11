784aa2cf4d96bb4d6a4be58edbc48fc5465a5cc2
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-11 11:39:11 +1000

    add new URL for CNW help center

--- js/git-update.js
+++ js/git-update.js
@@ -2132,3 +2132,10 @@ if (typeof(document.querySelector("#valueAccountRemaining")) != 'undefined' && d
 }
 
 
+if (window.location.href.indexOf("cnw.com.au") != -1){
+    const footerLink = document.getElementById("footerLinks-Customer-Support")
+    const footerLinkChildren = Array.from(footerLink.children);
+    const helpCenter = footerLinkChildren[0]
+    const anchorEle = helpCenter.querySelector('a')
+    anchorEle.href="https://help.cnw.com.au/"
+}

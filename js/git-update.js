4909c324b3b48e6bee37d51c2c070417103a944d
bgwgroup <webdeveloper@bgwgroup.com.au>
2023-10-05 14:28:02 +1000

    fixed another logic which prevents contact number from rendering

--- js/git-update.js
+++ js/git-update.js
@@ -2478,7 +2478,7 @@ class ClipsalClickFrenzy{
                             <label>Account Email</label>
                             <input type="text" name="account_email" value="${this.hiddenEmail.value}" readonly>
                         </div>
-                        ${this.hiddenEntries.value == "0" ? '<div class="form-row"><label>Contact Number</label><input type="text" name="account_contact_number" placeholder="Enter contact number"></div>' : ''}
+                        ${this.hiddenEntries.value != "0" ? '<div class="form-row"><label>Contact Number</label><input type="text" name="account_contact_number" placeholder="Enter contact number"></div>' : ''}
                         ${this.getHiddenMonthString(this.hiddenSelectedMonth.value) == 'October' && this.hiddenEntries.value != "0" ? '<div class="form-row form-select"><label>Bonus Entry(October)</label><select name="bonus_entry"><option value="no">No</option><option value="LSKD">Yes - LSKD</option><option value="PREEZZEE">Yes - PREZZEE</option></select></div>' : ''}
                         <div class="form-row form-select" style="${(this.hiddenEntries.value != "0") ? '' : 'display: none;'}">
                             <label>Voucher</label>

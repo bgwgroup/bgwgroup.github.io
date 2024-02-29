b676bdad17f66eca7f64d52bfa957a6fc0c9ca58
BGW Group <89062325+bgwgroup@users.noreply.github.com>
2024-03-01 08:31:43 +1000

    Update git-update.js

--- js/git-update.js
+++ js/git-update.js
@@ -2218,28 +2218,7 @@ class BigSupporter{
 }
 
 
-/**
- * TEMP hide invoices data on dashboard home
- */
-window.addEventListener('DOMContentLoaded', () => {
 
-     if (window.location.href.indexOf("/my-account") > -1) {
-
-         try {
-           let getInvoiceChartBlock = document.querySelector(".dashboard-stats-chart-invoices");
-            getInvoiceChartBlock.querySelector(".stats-block-left").style.display = "none";
-            getInvoiceChartBlock.querySelector(".stats-block-inner-split").style.display = "none";
-            getInvoiceChartBlock.querySelector(".stats").style.height = "100%";
-            getInvoiceChartBlock.querySelector(".stats-block-wrapper").style.display = "flex";
-            getInvoiceChartBlock.querySelector(".stats-block-wrapper").style.justifyContent = "center";
-            getInvoiceChartBlock.querySelector(".stats-block-wrapper").style.alignItems = "center";
-            getInvoiceChartBlock.querySelector(".stats-block-split").style.display = "flex";
-            getInvoiceChartBlock.querySelector(".stats-block-split").style.justifyContent = "center";
-            getInvoiceChartBlock.querySelector(".stats-block-split").style.alignItems = "center";
-        } catch (e) {}
-
-     }
-});
 
 /**
  * NEW EXPERIENCE LANDING PAGE SWIPER

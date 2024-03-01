0977d8eeab71b52d6130c3f7d0851db95c4782c4
BGW Group <89062325+bgwgroup@users.noreply.github.com>
2024-03-01 10:35:29 +1000

    Update git-update.js

--- js/git-update.js
+++ js/git-update.js
@@ -46,157 +46,138 @@
 //         });
 //     }
 // });
-console.log("update");
 
 // ============================== Sherriff Victoria landing page 2023 =============================================
 
-let sec5FAQaccordion = document.querySelectorAll(".sec-5-FAQs-accordion");
-if (!!sec5FAQaccordion) {
-    sec5FAQaccordion.forEach(element => {
-        element.addEventListener('click', () => {
-            let elementSibling = element.nextElementSibling; // get the next element after the current table header
-            if (elementSibling.style.maxHeight) { // check if maxHeight is set to a value greater than zero/null(0)
-                elementSibling.style.maxHeight = null; // return it back to null state
-            } else {
-                elementSibling.style.maxHeight = `${elementSibling.scrollHeight}px`; // if max-height is null/zero, expand the table body using the elements scrollHeight based on window position
-            }
-        })
-    })
-}
+// let sec5FAQaccordion = document.querySelectorAll(".sec-5-FAQs-accordion");
+// if (!!sec5FAQaccordion) {
+//     sec5FAQaccordion.forEach(element => {
+//         element.addEventListener('click', () => {
+//             let elementSibling = element.nextElementSibling; // get the next element after the current table header
+//             if (elementSibling.style.maxHeight) { // check if maxHeight is set to a value greater than zero/null(0)
+//                 elementSibling.style.maxHeight = null; // return it back to null state
+//             } else {
+//                 elementSibling.style.maxHeight = `${elementSibling.scrollHeight}px`; // if max-height is null/zero, expand the table body using the elements scrollHeight based on window position
+//             }
+//         })
+//     })
+// }
 
-let TestimonySlideIndex = 1;
-showS3TestimonySlides(TestimonySlideIndex);
-
-// Next/previous controls
-function plusS3TestimonySlides(n) {
-    showS3TestimonySlides(TestimonySlideIndex += n);
-}
-function showS3TestimonySlides(n) {
-    let testimonyS3Slides = document.getElementsByClassName("sec-3-testimony-slides");
-    let s3TestimonyDots = document.getElementsByClassName("s3-testimony-dot");
-    if (n > testimonyS3Slides.length) {TestimonySlideIndex = 1}
-    if (n < 1) {TestimonySlideIndex = testimonyS3Slides.length}
-    for (let i = 0; i < testimonyS3Slides.length; i++) {
-        testimonyS3Slides[i].style.display = "none";
-    }
-    for (let i = 0; i < s3TestimonyDots.length; i++) {
-        s3TestimonyDots[i].className = s3TestimonyDots[i].className.replace(" active", "");
-    }
-    if(!!testimonyS3Slides) {
-        if(!!Array.from(testimonyS3Slides)[TestimonySlideIndex-1]) {Array.from(testimonyS3Slides)[TestimonySlideIndex-1].style.display = "flex";}
-        if(!!Array.from(testimonyS3Slides)[TestimonySlideIndex-1]) {Array.from(testimonyS3Slides)[TestimonySlideIndex-1].style.justifyContent = "space-evenly";}
-        if(!!s3TestimonyDots[TestimonySlideIndex-1]) {s3TestimonyDots[TestimonySlideIndex-1].className += " active";}
-    }
-}
-
-let modalImageContainer = document.getElementById("modalImageContainer");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-let testimonyImages = document.querySelectorAll(".testimony-img");
-
-if (!!testimonyImages) {
-    for (let i = 0; i < testimonyImages.length; i++ ) {
-        let modalTestimonyImg = document.getElementById("modal-testimony-img");
-        testimonyImages[i].onclick = function(){
-        modalImageContainer.style.display = "block";
-        modalTestimonyImg.src = this.src;
-        }
-    }
-}
-
-// Get the <span> element that closes the modal
-let closeTestimonyBtn = document.getElementsByClassName("close-testimony-img")[0];
-
-// When the user clicks on <span> (x), close the modal
-if (!!closeTestimonyBtn) {
-    closeTestimonyBtn.onclick = function() {
-        modalImageContainer.style.display = "none";
-    }
-}
+// let TestimonySlideIndex = 1;
+// showS3TestimonySlides(TestimonySlideIndex);
+// function plusS3TestimonySlides(n) {
+//     showS3TestimonySlides(TestimonySlideIndex += n);
+// }
+// function showS3TestimonySlides(n) {
+//     let testimonyS3Slides = document.getElementsByClassName("sec-3-testimony-slides");
+//     let s3TestimonyDots = document.getElementsByClassName("s3-testimony-dot");
+//     if (n > testimonyS3Slides.length) {TestimonySlideIndex = 1}
+//     if (n < 1) {TestimonySlideIndex = testimonyS3Slides.length}
+//     for (let i = 0; i < testimonyS3Slides.length; i++) {
+//         testimonyS3Slides[i].style.display = "none";
+//     }
+//     for (let i = 0; i < s3TestimonyDots.length; i++) {
+//         s3TestimonyDots[i].className = s3TestimonyDots[i].className.replace(" active", "");
+//     }
+//     if(!!testimonyS3Slides) {
+//         if(!!Array.from(testimonyS3Slides)[TestimonySlideIndex-1]) {Array.from(testimonyS3Slides)[TestimonySlideIndex-1].style.display = "flex";}
+//         if(!!Array.from(testimonyS3Slides)[TestimonySlideIndex-1]) {Array.from(testimonyS3Slides)[TestimonySlideIndex-1].style.justifyContent = "space-evenly";}
+//         if(!!s3TestimonyDots[TestimonySlideIndex-1]) {s3TestimonyDots[TestimonySlideIndex-1].className += " active";}
+//     }
+// }
+// let modalImageContainer = document.getElementById("modalImageContainer");
+// let testimonyImages = document.querySelectorAll(".testimony-img");
+// if (!!testimonyImages) {
+//     for (let i = 0; i < testimonyImages.length; i++ ) {
+//         let modalTestimonyImg = document.getElementById("modal-testimony-img");
+//         testimonyImages[i].onclick = function(){
+//         modalImageContainer.style.display = "block";
+//         modalTestimonyImg.src = this.src;
+//         }
+//     }
+// }
+// let closeTestimonyBtn = document.getElementsByClassName("close-testimony-img")[0];
+// if (!!closeTestimonyBtn) {
+//     closeTestimonyBtn.onclick = function() {
+//         modalImageContainer.style.display = "none";
+//     }
+// }
 
 // Rinnai LP Cash promo
 
-const modalRinnai = document.getElementById("rinnai-promo-modal");
-const imgVideoRinnai = document.getElementById("rinnai-cash-promo-img");
-const cashSplashVideoRinnai = document.getElementById("cash-video");
-const rinnaiSpanCloseBtn = document.getElementsByClassName("rinnai-promo-close")[0];
-const rinnaiViewportWidth = window.innerWidth;
+// const modalRinnai = document.getElementById("rinnai-promo-modal");
+// const imgVideoRinnai = document.getElementById("rinnai-cash-promo-img");
+// const cashSplashVideoRinnai = document.getElementById("cash-video");
+// const rinnaiSpanCloseBtn = document.getElementsByClassName("rinnai-promo-close")[0];
+// const rinnaiViewportWidth = window.innerWidth;
 
-if (!!imgVideoRinnai) {imgVideoRinnai.onclick = function(){
-  modalRinnai.style.display = "block";
-  if(rinnaiViewportWidth < 769) {
-    cashSplashVideoRinnai.style.display = "none"
-  }
-  if(rinnaiViewportWidth > 769) {
-    cashSplashVideoRinnai.play();
-  }
-}}
+// if (!!imgVideoRinnai) {imgVideoRinnai.onclick = function(){
+//   modalRinnai.style.display = "block";
+//   if(rinnaiViewportWidth < 769) {
+//     cashSplashVideoRinnai.style.display = "none"
+//   }
+//   if(rinnaiViewportWidth > 769) {
+//     cashSplashVideoRinnai.play();
+//   }
+// }}
 
-if (!!rinnaiSpanCloseBtn) {
-  rinnaiSpanCloseBtn.onclick = function() {
-    modalRinnai.style.display = "none";
-  }
-}
+// if (!!rinnaiSpanCloseBtn) {
+//   rinnaiSpanCloseBtn.onclick = function() {
+//     modalRinnai.style.display = "none";
+//   }
+// }
 
 // Hager Promo LP
-
-const hagerStepElements = Array.from(document.querySelectorAll('.sec2-hager-step'));
-function animateElements() {
-    hagerStepElements.forEach((element, index) => {
-        // Calculate the delay for each element
-        const delay = index * 1000; // Adjust the delay as needed
-        // Apply animation to move elements from right to left
-        setTimeout(() => {
-        element.style.transform = 'translateX(0%)';
-        element.style.opacity = "1";
-        }, delay);
-    });
-}
-
+// const hagerStepElements = Array.from(document.querySelectorAll('.sec2-hager-step'));
+// function animateElements() {
+//     hagerStepElements.forEach((element, index) => {
+//         // Calculate the delay for each element
+//         const delay = index * 1000; // Adjust the delay as needed
+//         // Apply animation to move elements from right to left
+//         setTimeout(() => {
+//         element.style.transform = 'translateX(0%)';
+//         element.style.opacity = "1";
+//         }, delay);
+//     });
+// }
 // Call the animateElements function after a delay
-if(!!hagerStepElements) {
-setTimeout(animateElements, 1200); // Adjust the delay as needed
-}
-
-const zoomOutHagerElement = document.querySelector('.sec1-hager-hero-slider');
-function zoomOut() {
+// if(!!hagerStepElements) {
+// setTimeout(animateElements, 1200); // Adjust the delay as needed
+// }
+// const zoomOutHagerElement = document.querySelector('.sec1-hager-hero-slider');
+// function zoomOut() {
 // Set initial zoom scale
-zoomOutHagerElement.style.transform = 'scale(2)';
-
+// zoomOutHagerElement.style.transform = 'scale(2)';
 // Delay the animation to allow the initial layout to render
-setTimeout(() => {
-    // Apply animation to zoom out the element
-    zoomOutHagerElement.style.transform = 'scale(1)';
-    }, 0); // Adjust the delay as needed
-}
-
-if (!!zoomOutHagerElement) {
-    window.addEventListener('load', zoomOut)
-}
-
+// setTimeout(() => {
+//     // Apply animation to zoom out the element
+//     zoomOutHagerElement.style.transform = 'scale(1)';
+//     }, 0); // Adjust the delay as needed
+// }
+// if (!!zoomOutHagerElement) {
+//     window.addEventListener('load', zoomOut)
+// }
 
 // ------ Big Local Supporter Landing page ----
-
-const bigSupporterBanner = document.querySelector('.big-supporter-sec1-ctn img');
-const bigSupporterPreloader = document.querySelector('.big-supporter-sec1-preloader')
-const bigSuppporterSec2 = document.querySelector(".big-supporter-sec2-maincontain-ctn")
-const bigSuppporterSec3 = document.querySelector('.big-supporter-sec3-main-ctn')
-function hideBigbanner() {
-    setTimeout(() => {
-        bigSupporterBanner.style.transform = "scale(0)"
-    }, 1800)
-    setTimeout(() => {
-        bigSupporterPreloader.style.display = "none";
-        bigSuppporterSec2.style.display = "block";
-        bigSuppporterSec2.style.transform = "scale(1)";
-        bigSuppporterSec2.style.height = "100%";
-        bigSuppporterSec3.style.display = 'block';
-    }, 2100)
-}
-
-if (!!bigSupporterPreloader) {
-    window.addEventListener('load', hideBigbanner)
-}
+// const bigSupporterBanner = document.querySelector('.big-supporter-sec1-ctn img');
+// const bigSupporterPreloader = document.querySelector('.big-supporter-sec1-preloader')
+// const bigSuppporterSec2 = document.querySelector(".big-supporter-sec2-maincontain-ctn")
+// const bigSuppporterSec3 = document.querySelector('.big-supporter-sec3-main-ctn')
+// function hideBigbanner() {
+//     setTimeout(() => {
+//         bigSupporterBanner.style.transform = "scale(0)"
+//     }, 1800)
+//     setTimeout(() => {
+//         bigSupporterPreloader.style.display = "none";
+//         bigSuppporterSec2.style.display = "block";
+//         bigSuppporterSec2.style.transform = "scale(1)";
+//         bigSuppporterSec2.style.height = "100%";
+//         bigSuppporterSec3.style.display = 'block';
+//     }, 2100)
+// }
+// if (!!bigSupporterPreloader) {
+//     window.addEventListener('load', hideBigbanner)
+// }
 
 //Cnw Only Big One Banner Inject
 /*
